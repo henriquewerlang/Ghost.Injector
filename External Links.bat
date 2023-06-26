e49fb76d8178293c93e21252359315b73058d4df
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2023-06-26 20:03:18 -0300

    Update the externals and renamed the mock repository to the new name.

--- External Links.bat
+++ External Links.bat
@@ -2,8 +2,8 @@ mkdir Externals
 
 cd Externals
 
-mklink /j Delphi.Mock %1"\Delphi.Mock"
 mklink /j DUnitX %1"\DUnitX"
 mklink /j FastMM5 %1"\FastMM5"
+mklink /j Translucent %1"\Translucent"
 
 cd..
